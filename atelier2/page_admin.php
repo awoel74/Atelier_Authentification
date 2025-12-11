995e93450c6f1d5a9304081015c44b94b14f7a6d
awoel74 <herilaza.woel-lala@eleve.isep.fr>
2025-12-11 11:28:14 +0100

    Update page_admin.php

--- atelier2/page_admin.php
+++ atelier2/page_admin.php
@@ -1,25 +1,21 @@
 <?php
-// Démarrer la session
 session_start();
 
-// Vérifier si l'utilisateur est bien en possession d'un cookie valide
-// Dans le cas contraire il sera redirigé vers la page d'accueil de connexion
 if (!isset($_COOKIE['authToken']) || $_COOKIE['authToken'] !== '12345') {
     header('Location: index.php');
     exit();
 }
 ?>
-
 <!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Accueil</title>
+    <title>Page admin</title>
 </head>
 <body>
-    <h1>Bienvenue sur la page Administrateur protégée par un Cookie</h1>
-    <p>Vous êtes connecté en tant qu'admin.</p>
-    <a href="logout.php">Se déconnecter</a>
+    <h1>Bienvenue sur la page ADMIN</h1>
+    <p>Vous êtes connecté via un cookie valable 1 minute.</p>
+
+    <p><a href="logout.php">Se déconnecter</a></p>
 </body>
 </html>
