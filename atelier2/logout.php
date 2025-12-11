c414d2a7ec22ea9b68f30f6f327a4688ffcae64d
awoel74 <herilaza.woel-lala@eleve.isep.fr>
2025-12-11 11:37:40 +0100

    Update logout.php

--- atelier2/logout.php
+++ atelier2/logout.php
@@ -1,5 +1,13 @@
 <?php
 session_start();
+
+// On vide la session
+$_SESSION = [];
+session_destroy();
+
+// On supprime le cookie
 setcookie('authToken', '', time() - 3600, '/', '', false, true);
+
+// Retour à la page de login
 header('Location: index.php');
 exit();
